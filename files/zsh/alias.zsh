6d22f548b1ae04464d031cc284f902b479db5907
Fernando Levin <flevin58@gmail.com>
2025-11-07 00:48:16 +0100

    added llvm and clang

---
+++ files/zsh/alias.zsh
@@ -0,0 +1,19 @@
+# Add basic aliases
+# alias ls="eza --icons=always"
+alias ls="ls --color"
+alias ll="ls -l"
+alias lt="ls -lt"
+alias la="ls -al"
+alias godev="cd $PROJECTS"
+alias gopro="cd $PROJECTS"
+alias godot="cd $DOTFILES"
+# Add more advanced aliases
+alias edit="$EDITOR"
+alias remove="rm -rf"
+alias reload="source ~/.zshrc"
+alias vim=nvim
+alias vienv="vim ~/.env"
+alias vidot="vim $DOTFILES"
+alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
+alias tree="tree -C"
+alias gtree="tree --gitignore"
