5847a8d33079580e69d1a6f1aefedd4b50ddc65e
Fernando Julio Levin <flevin58@gmail.com>
2026-04-02 22:12:18 +0200

    Updated zsh

--- files/home/config/zsh/alias-go.zsh
+++ files/home/config/zsh/alias-go.zsh
@@ -1,8 +1,36 @@
-#====================================
-# Ansible generated file, DO NOT EDIT
-#====================================
+# Add go-related aliases
 alias gor="go run ."
 alias got="go test -v ./..."
 alias goi="go install"
 alias gou="go clean -i"
 alias gob="go build"
+alias gogo="cd $PROJECTS/Go"
+
+# Add go-related functions
+function gonew {
+    base="github.com/flevin58"
+    here=$(pwd)
+    case "$1" in
+    "")
+        if [[ -f "$here/go.mod" ]]; then
+            echo "go.mod already exists"
+            return
+        fi
+        go mod init "$base/$(basename $here)"
+        ;;
+    *)
+        if [[ -d $1 ]]; then
+            echo "Folder $1 already exists"
+            return
+        fi
+        if [[ "$1" == "$(basename $here)" ]]; then
+            echo "You are aleady in folder $1"
+            return
+        fi
+        mkdir $1
+        chdir $1
+        go mod init "$base/$1"
+        git init
+        ;;
+    esac
+}
