b6e928034354bded41ec0a61a48d900ded2d9c2f
Fernando Levin <flevin58@gmail.com>
2026-04-16 18:04:36 +0200

    Added eza

--- playbooks/files/home/config/zsh/alias.zsh
+++ playbooks/files/home/config/zsh/alias.zsh
@@ -1,19 +1,27 @@
 #====================================
 # Ansible generated file, DO NOT EDIT
 #====================================
-# alias ls="eza --icons=always"
-alias ls="ls --color"
+# Modern ls & tree alternatives with eza
+alias ls="eza --icons=auto"
 alias ll="ls -l"
 alias lt="ls -lt"
 alias la="ls -al"
+# git-ignore vbersions of the above
+alias gls="ls --git-ignore"
+alias gll="gls -l"
+alias glt="gls -lt"
+alias gla="gls -al"
+# tree versions
+alias tree="eza --tree"
+alias gtree="tree --git-ignore"
+# Folder shortcuts
 alias godev="cd $PROJECTS"
 alias gopro="cd $PROJECTS"
+alias goconf="cd $HOME/.config"
 # Add more advanced aliases
 alias edit="$EDITOR"
 alias remove="rm -rf"
 alias reload="source ~/.zshrc"
 alias vim=nvim
 alias vienv="vim ~/.env"
-alias tree="tree -C"
-alias gtree="tree --gitignore"
 alias brewup="brew update && brew upgrade"
